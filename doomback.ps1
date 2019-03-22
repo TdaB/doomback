88ecc87e185c9b84445e5b4ecf8d99119edb748f
TdaB <tmegantz@gmail.com>
2019-03-21 18:03:38 -0700

    Initial commit of doom backup script

---
+++ doomback.ps1
@@ -0,0 +1,10 @@
+﻿param (
+    [Parameter(Mandatory = $True)]
+    [string]$path
+)
+
+$Timestamp = Get-Date -Format o | foreach {$_ -replace ":", "."}
+$SourcePath = "$env:USERPROFILE\Saved Games\id Software\DOOM\base\savegame.user\"
+$DestPath = Join-Path $path $Timestamp
+Write-Output "Backing up to $DestPath"
+Copy-Item $SourcePath $DestPath -recurse
